992b291f121624770303e0782366b4d99255d986
Bralco Creations <info@bralcocreations.co.ke>
2020-05-22 15:39:10 +0300

    Timeline Plugin Update 0.0.5

---
+++ src/components/home/plugins/TimelineComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <timeline-view/>
+</template>
+
+<script>
+    import timeline from 'timeline-vuejs'
+
+    export default {
+        name: "timeline",
+        components: {
+            "timeline-view": timeline
+        },
+        props: ["items"],
+        data () {
+            return {
+                messageWhenNoItems: 'There arent items',
+            }
+        }
+    }
+</script>
