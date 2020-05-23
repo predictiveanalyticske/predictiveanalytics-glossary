a6a3a9f87c5ca6d233a574a64919ae11d7097e82
Bralco Creations <info@bralcocreations.co.ke>
2020-05-23 12:46:24 +0300

    Category Filter Update 0.0.14

--- src/components/home/show/CategoryFilterComponent.vue
+++ src/components/home/show/CategoryFilterComponent.vue
@@ -3,45 +3,87 @@
         <div>
             <navbar></navbar>
         </div>
-        <div>
-            <timeline/>
+        <div class="uk-margin-remove">
+            <vk-card class="uk-card-red uk-card-no-shadow uk-light uk-padding-large">
+                <vk-grid class="uk-child-width-1-2@xl uk-child-width-1-2@l uk-child-width-1-2@m uk-child-width-1-1@s">
+                <div>
+                    <h1>{{ glossary.title }}</h1>
+                    <h5 class="uk-margin-remove">{{ glossary.summary }}</h5>
+                </div>
+                </vk-grid>
+            </vk-card>
+        </div>
+        <div class="uk-margin-remove">
+            <vk-card type="blank">
+                <main>
+                    <div class="item" v-for="(value,index) in activePage" :key="index">
+                        <h2>{{ value.name }}</h2>
+                        <p>{{ value.definition }}</p>
+                    </div>
+                </main>
+                <div>
+                    <vk-pagination align="center" :page.sync="pageNumber" :range="1" :perPage="perPage" :total="tableLength.value">
+                        <vk-pagination-page-prev></vk-pagination-page-prev>
+                        <vk-pagination-pages></vk-pagination-pages>
+                        <vk-pagination-page-next></vk-pagination-page-next>
+                    </vk-pagination>
+                </div>
+            </vk-card>
         </div>
     </vk-grid>
 </template>
 
 <script>
+
     import navbar from '../headers/NavbarComponent'
-    import timeline from '../plugins/TimelineComponent'
     import { chunk } from 'lodash'
 
     export default {
         name: "index",
         components: {
             navbar,
-            timeline
         },
         data () {
             return {
                 activePage: {},
                 data: {},
                 glossaryChunk: {},
+                glossary: {},
                 pages: 0,
-                perPage: 5
-                
+                pageNumber: 1,
+                perPage: 10,
+                tableLength: {
+                    type: Number,
+                    value: 0
+                }
             }
         },
-        created() {
+        methods: {
+            makePage (){
+                if( this.data.length > 0){
+                    this.pages  = this.data.length % this.perPage
+                    this.glossaryChunk = chunk(this.data, this.perPage);
+                    this.activePage = this.glossaryChunk[this.pageNumber - 1];
+                    this.tableLength.value = this.data.length;
+                }
+            }
+        },
+        beforeCreate() {
             this.bralcoaxios({ url: process.env.VUE_APP_ENDPOINT_URL + "/api/v1/glossary/fetch/view/" + this.$route.params.category, request: "GET" }).then( (response) => {
                 let resolve = this.bralcoresponse(response);
                 this.data   = resolve.data.items;
-                this.pages  = this.data.length % this.perPage
-                if( this.data.length > 0){
-                    // this.data = Object.entries(this.data).map( ( [, value] ) => { return value; } );
-                    this.glossaryChunk = chunk(this.data, this.perPage);
-                    this.activePage = this.glossaryChunk[1];
-                    this.tableLength.value = this.activePage.length;
-                }
+                this.glossary  = resolve.data.glossary;
+                this.makePage();
             });
+        },
+        watch:{
+            pageNumber () {
+                this.activePage = this.glossaryChunk[this.pageNumber - 1];
+            }
         }
     }
 </script>
+
+<style lang="scss">
+    @import "@/assets/css/timeline.scss"
+</style>
