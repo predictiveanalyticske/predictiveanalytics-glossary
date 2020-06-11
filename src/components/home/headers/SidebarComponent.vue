9ca2b7ffb652d480577b97255e1a6448e20a27c1
Bralco Creations <info@bralcocreations.co.ke>
2020-06-11 23:09:26 +0300

    Sidebar Update 0.1.9

---
+++ src/components/home/headers/SidebarComponent.vue
@@ -0,0 +1,32 @@
+<template>
+<vk-offcanvas-content>
+  <vk-offcanvas :show.sync="show">
+     <vk-offcanvas-close @click="$store.commit('sidebar',false)"></vk-offcanvas-close>
+    <vk-nav class="uk-margin-auto-vertical">
+      <vk-nav-item title="Active" active></vk-nav-item>
+      <vk-nav-item title="Item"></vk-nav-item>
+      <vk-nav-item-header title="Header"></vk-nav-item-header>
+      <vk-nav-item title="Item"></vk-nav-item>
+      <vk-nav-item title="Item"></vk-nav-item>
+      <vk-nav-item-divider></vk-nav-item-divider>
+      <vk-nav-item title="Item"></vk-nav-item>
+    </vk-nav>
+  </vk-offcanvas>
+</vk-offcanvas-content>
+</template>
+
+<script>
+    export default {
+        name: "sidebar",
+        data () {
+            return {
+                
+            }
+        },
+        computed:{
+            show () {
+                return this.$store.getters.sidebar;
+            }
+        }
+    }
+</script>
